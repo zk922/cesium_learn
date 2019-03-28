0c38dcf1c262b6a12ceeccddf875575f71ac3d90
zk922 <zhangk922@163.com>
2019-03-28 22:10:21 +0800

    webpack config

---
+++ build.js
@@ -0,0 +1,20 @@
+const webpack = require('webpack');
+
+const config = require('./webpack.config');
+
+let compiler = webpack(config);
+
+compiler.watch({
+  aggregateTimeout: 300,
+  poll: undefined
+}, function (err, stats) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  console.log(stats.toString({
+    chunks: false,  // Makes the build much quieter
+    colors: true    // Shows colors in the console
+  }));
+});
---
+++ src/main.js
@@ -0,0 +1,3 @@
+let cesium = require('cesium/Source/Cesium');
+
+console.log(cesium);
